620f80ca49620cddc842f7925838b7d058e4611e
Arkadiusz Staśko <arek.stasko@gmail.com>
2023-12-05 21:43:33 +0100

    Add connection to plantcare server

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <WiFi.h>
 #include <Arduino.h>
+#include <HTTPClient.h>
 
 #define WIFI_SSID ""
 #define WIFI_PASSWORD ""
+#define API_BASE_URL ""
 
 void setup() {
   Serial.begin(921600);
@@ -14,6 +16,7 @@ void setup() {
 }
 
 bool isConnected = false;
+bool isRegistered = false;
 
 void loop() {
   if(WiFi.status() == WL_CONNECTED && !isConnected){
@@ -27,5 +30,18 @@ void loop() {
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
     isConnected = false;
     delay(1000);
+  }
+
+  if(!isRegistered){
+    Serial.println("START REGISTERING MODULE");
+    HTTPClient client;
+    client.begin(String(API_BASE_URL) + "/modules/Add");
+    int httpCode = client.POST("");
+    if(httpCode > 0) {
+      String payload = client.getString();
+      Serial.println("\nStatuscode: " + String(httpCode));
+      Serial.println(payload);
+    }
+    isRegistered = true;
   }
 }
