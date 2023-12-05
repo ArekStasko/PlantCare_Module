e6de5193e5f5f38dc0a2973b755de4b1b52542de
Arkadiusz Staśko <arek.stasko@gmail.com>
2023-12-05 21:06:08 +0100

    Add wifi connection

--- README.md
+++
@@ -1,9 +0,0 @@
-# PlantCare_Module
-
-This is code that will work on ESP32 module to communicate with my PlantCare system.
-
-The main operations that it will perform are : 
-- Gaining informations about soil moisture level
-- An exchange of information with PlantCare server
-
-In the next days i will write more details.
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
+#include <WiFi.h>
 #include <Arduino.h>
 
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
 void setup() {
+  Serial.begin(921600);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  Serial.println("starting");
 }
 
-void loop() {
+bool isConnected = false;
 
+void loop() {
+  if(WiFi.status() == WL_CONNECTED && !isConnected){
+    Serial.println("connected");
+    digitalWrite(LED_BUILTIN, HIGH);
+    isConnected = true;
+  }
+
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.println(".");
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    isConnected = false;
+    delay(1000);
+  }
 }
