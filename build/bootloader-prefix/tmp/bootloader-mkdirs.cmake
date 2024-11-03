c3114c8c1be0f7c8a1b943b51afe51019163c50e
ArekStasko <arek.stasko@gmail.com>
2024-11-03 18:07:53 +0100

    Add led blink to test conf

--- build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
+++ build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -5,18 +5,18 @@ cmake_minimum_required(VERSION 3.5)
 
 file(MAKE_DIRECTORY
   "/home/arekstasko/sources/Espressif-IDE/esp-idf-v5.3.1/components/bootloader/subproject"
-  "/home/arekstasko/sources/repos/PlantCare_Module/PlantCare_Module/build/bootloader"
-  "/home/arekstasko/sources/repos/PlantCare_Module/PlantCare_Module/build/bootloader-prefix"
-  "/home/arekstasko/sources/repos/PlantCare_Module/PlantCare_Module/build/bootloader-prefix/tmp"
-  "/home/arekstasko/sources/repos/PlantCare_Module/PlantCare_Module/build/bootloader-prefix/src/bootloader-stamp"
-  "/home/arekstasko/sources/repos/PlantCare_Module/PlantCare_Module/build/bootloader-prefix/src"
-  "/home/arekstasko/sources/repos/PlantCare_Module/PlantCare_Module/build/bootloader-prefix/src/bootloader-stamp"
+  "/home/arekstasko/sources/repos/PlantCare_Module/build/bootloader"
+  "/home/arekstasko/sources/repos/PlantCare_Module/build/bootloader-prefix"
+  "/home/arekstasko/sources/repos/PlantCare_Module/build/bootloader-prefix/tmp"
+  "/home/arekstasko/sources/repos/PlantCare_Module/build/bootloader-prefix/src/bootloader-stamp"
+  "/home/arekstasko/sources/repos/PlantCare_Module/build/bootloader-prefix/src"
+  "/home/arekstasko/sources/repos/PlantCare_Module/build/bootloader-prefix/src/bootloader-stamp"
 )
 
 set(configSubDirs )
 foreach(subDir IN LISTS configSubDirs)
-    file(MAKE_DIRECTORY "/home/arekstasko/sources/repos/PlantCare_Module/PlantCare_Module/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+    file(MAKE_DIRECTORY "/home/arekstasko/sources/repos/PlantCare_Module/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
 endforeach()
 if(cfgdir)
-  file(MAKE_DIRECTORY "/home/arekstasko/sources/repos/PlantCare_Module/PlantCare_Module/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+  file(MAKE_DIRECTORY "/home/arekstasko/sources/repos/PlantCare_Module/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
 endif()
--- main/PlantCare_Module.c
+++ main/PlantCare_Module.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <driver/gpio.h>
+
+#define LED_GPIO_PIN 2
 
 void app_main(void)
 {
+    esp_rom_gpio_pad_select_gpio(LED_GPIO_PIN);
+    gpio_set_direction(LED_GPIO_PIN, GPIO_MODE_OUTPUT);
 
+    while (true) {
+        gpio_set_level(LED_GPIO_PIN, 1);
+        printf("LED ON\n");
+
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+
+        gpio_set_level(LED_GPIO_PIN, 0);
+        printf("LED OFF\n");
+
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
 }
