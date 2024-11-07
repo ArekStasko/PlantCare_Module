cad748ac3d447cddfb453c8808b74b3f2b222fb3
ArekStasko <arek.stasko@gmail.com>
2024-11-07 20:27:57 +0100

    Initialize ble

--- main/PlantCare_Module.c
+++ main/PlantCare_Module.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "esp_bt_defs.h"
-PRIV_REQUIRES bt
+#include "esp_bt_main.h"
+#include "esp_bt_device.h"
+
+#define PLANT_CARE_DEVICE_NAME "PLANT_CARE_DEVICE"
+
 void app_main(void)
 {
-    esp_bluedroid_enable(void);
+    esp_bluedroid_enable();
+    esp_bluedroid_init();
+    esp_bt_dev_set_device_name(PLANT_CARE_DEVICE_NAME);
+    while (1)
+    {
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      int status = esp_bluedroid_get_status();
+      printf("status=%d\n", status);
+    }
 }
