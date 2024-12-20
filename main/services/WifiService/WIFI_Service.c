4649e4a795ebd2fcc30007022f583ea3d1e45227
ArekStasko <arek.stasko@gmail.com>
2024-12-20 15:49:16 +0100

    Add wifi initialization

--- main/services/WifiService/WIFI_Service.c
+++ main/services/WifiService/WIFI_Service.c
@@ -7,7 +7,15 @@ void connectToWifi()
 {
   	char* wifiName = getWifiName();
     char* wifiPassword = getWifiPassword();
-    printf("Wifi name: %s\n", wifiName);
-    printf("Wifi password: %s\n", wifiPassword);
-    printf("Connecting to WiFi network...");
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_err_t ret = esp_wifi_init(&cfg);
+    if(ret != ESP_OK)
+    {
+      printf("Failed to initialize WiFi network\n");
+    }
+	ret = esp_wifi_set_mode(WIFI_MODE_STA);
+    if(ret != ESP_OK)
+    {
+      printf("Failed to set WiFi as STA mode\n");
+    }
 }
