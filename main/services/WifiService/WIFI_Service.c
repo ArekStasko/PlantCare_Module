16cb88c5bb65c34393744bd4e9ff47e4f4b3e23f
ArekStasko <arek.stasko@gmail.com>
2026-01-06 18:03:54 +0100

    FRTOS task

--- main/services/WifiService/WIFI_Service.c
+++ main/services/WifiService/WIFI_Service.c
@@ -11,66 +11,6 @@
 #include "Sensor_Service.h"
 #include "esp_netif_ip_addr.h"
 #include "esp_http_client.h"
-#define MAX_RESPONSE_LENGTH 64
-
-static void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
-{
-    switch (event_id)
-    {
-    case WIFI_EVENT_STA_START:
-        printf("WiFi connecting WIFI_EVENT_STA_START ... \n");
-        break;
-    case WIFI_EVENT_STA_CONNECTED:
-        printf("WiFi connected WIFI_EVENT_STA_CONNECTED ... \n");
-        break;
-    case WIFI_EVENT_STA_DISCONNECTED:
-        printf("WiFi lost connection WIFI_EVENT_STA_DISCONNECTED ... \n");
-        esp_wifi_connect();
-        break;
-    case IP_EVENT_STA_GOT_IP:
-        {
-        	ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
-        	char ip_str[16];
-        	esp_ip4addr_ntoa(&event->ip_info.ip, ip_str, sizeof(ip_str));
-        	printf("IP ADDRESS: %s\n", ip_str);
-
-            nvs_handle_t nvs_handle;
-        	esp_err_t err = nvs_open("storage", NVS_READWRITE, &nvs_handle);
-        	if (err != ESP_OK)
-        	{
-                printf("UNDABLE TO OPEN NVS STORAGE \n");
-            	break;
-        	}
-            nvs_set_str(nvs_handle, "address", ip_str);
-        	break;
-    	}
-    default:
-        break;
-    }
-}
-
-void connect_to_wifi()
-{
-  	char* wifiName = getWifiName();
-    char* wifiPassword = getWifiPassword();
-
-    esp_netif_init();
-    esp_event_loop_create_default();
-    esp_netif_create_default_wifi_sta();
-    wifi_init_config_t wifi_initiation = WIFI_INIT_CONFIG_DEFAULT();
-    esp_wifi_init(&wifi_initiation);
-    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler, NULL);
-    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL);
-
-    wifi_config_t wifi_configuration = {0};
-    snprintf((char*)wifi_configuration.sta.ssid, sizeof(wifi_configuration.sta.ssid), "%s", wifiName);
-    snprintf((char*)wifi_configuration.sta.password, sizeof(wifi_configuration.sta.password), "%s", wifiPassword);
-
-    esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_configuration);
-    esp_wifi_set_mode(WIFI_MODE_STA);
-    esp_wifi_start();
-    esp_wifi_connect();
-}
 
 static void send_moisture_to_server(void)
 {
@@ -104,3 +44,62 @@ static void send_moisture_to_server(void)
     esp_http_client_cleanup(client);
 }
 
+
+static void moisture_task(void *pvParameter)
+{
+    while (1) {
+        send_moisture_to_server();
+        vTaskDelay(pdMS_TO_TICKS(600000));
+    }
+}
+
+static void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+{
+    switch (event_id)
+    {
+    case WIFI_EVENT_STA_START:
+        printf("WiFi connecting WIFI_EVENT_STA_START ... \n");
+        break;
+    case WIFI_EVENT_STA_CONNECTED:
+        printf("WiFi connected WIFI_EVENT_STA_CONNECTED ... \n");
+        break;
+    case WIFI_EVENT_STA_DISCONNECTED:
+        printf("WiFi lost connection WIFI_EVENT_STA_DISCONNECTED ... \n");
+        esp_wifi_connect();
+        break;
+    case IP_EVENT_STA_GOT_IP:
+        {
+        	static bool task_created = false;
+            if (!task_created) {
+                xTaskCreate(moisture_task, "MoistureTask", 4096, NULL, 5, NULL);
+                task_created = true;
+            }
+        	break;
+    	}
+    default:
+        break;
+    }
+}
+
+void connect_to_wifi()
+{
+  	char* wifiName = getWifiName();
+    char* wifiPassword = getWifiPassword();
+
+    esp_netif_init();
+    esp_event_loop_create_default();
+    esp_netif_create_default_wifi_sta();
+    wifi_init_config_t wifi_initiation = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&wifi_initiation);
+    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler, NULL);
+    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL);
+
+    wifi_config_t wifi_configuration = {0};
+    snprintf((char*)wifi_configuration.sta.ssid, sizeof(wifi_configuration.sta.ssid), "%s", wifiName);
+    snprintf((char*)wifi_configuration.sta.password, sizeof(wifi_configuration.sta.password), "%s", wifiPassword);
+
+    esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_configuration);
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_start();
+    esp_wifi_connect();
+}
